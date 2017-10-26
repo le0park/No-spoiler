6d034caa68a5410de345611f5b5808618233520c
Junyoung Park <carpe0308@naver.com>
2017-10-26 11:37:18 +0900

    first commit

---
+++ getSource.js
@@ -0,0 +1,23 @@
+var xhr = new XMLHttpRequest();
+var query = '범죄도시';
+var clientId = 'z3PC0R7c9QUP9GqOXffK';
+var clientSecret = 'DKRegT6p7Z';
+var resp;
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && this.status == 200){
+        resp = this.response;
+    }
+}
+xhr.open("GET", 'https://openapi.naver.com/v1/search/movie.json' + '?query=' + query, true);
+xhr.setRequestHeader("X-Naver-Client-Id", clientId);
+xhr.setRequestHeader("X-Naver-Client-Secret", clientSecret);
+xhr.send();
+
+function get_source(document_body){
+    return document_body.innerText;
+}
+
+chrome.extension.sendMessage({
+    action: "getSource",
+    source: resp
+});
