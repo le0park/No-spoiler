607e16781b6a95ce96e10006d21363ecb571c7f8
simhongsub <oxhe2038@naver.com>
2017-10-28 22:29:13 +0900

    html을 통한 검색

--- popup.js
+++ popup.js
@@ -1,16 +1,18 @@
-chrome.extension.onMessage.addListener(function(request, sender) {
-    if (request.action == "getSource") {
-        document.body.innerText = request.source;
-    }
-});
+var query;
 
 function onWindowLoad() {
-    chrome.tabs.executeScript(null, {
-        file: "getSource.js"
-        }, function() {
-            if (chrome.extension.lastError) {
-                document.body.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
-            }
-        });
+  document.getElementById("search").addEventListener("click", search_movie);
 }
+
+function search_movie(){
+  query = document.getElementById('query').value;
+  chrome.tabs.executeScript(null, {
+    file: "getSource.js"
+    }, function() {
+      if (chrome.extension.lastError) {
+          document.body.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
+      }
+  });
+};
+
 window.onload = onWindowLoad;
