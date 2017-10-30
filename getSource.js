cf7bfe6a721ed7efd8de36ce86e1dff45f812003
Junyoung Park <carpe0308@naver.com>
2017-10-30 19:08:09 +0900

    query 입력할 때 유니코드로 반환되어 검색 안되는 것 수정

--- getSource.js
+++ getSource.js
@@ -10,7 +10,8 @@ function onWindowLoad() {
 function search_movie(){
   var query = document.getElementById('query').value;
 
-  xhr.open("GET", 'https://openapi.naver.com/v1/search/movie.json' + '?query=' + query, true);
+  xhr.open("GET", 'https://openapi.naver.com/v1/search/movie.json' 
+                + '?query=' + encodeURI(query) , true);
   xhr.setRequestHeader("X-Naver-Client-Id", clientId);
   xhr.setRequestHeader("X-Naver-Client-Secret", clientSecret);
   xhr.send();
@@ -19,7 +20,7 @@ function search_movie(){
       if(xhr.readyState == 4 && this.status == 200){
           resp = this.response;
           if (resp !== undefined){
-              document.body.innerText = resp.source;
+              document.body.innerText = resp;
               }
           }
       }
