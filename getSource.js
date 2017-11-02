01a1cbad2523d0026dd7320589a83c6e71832aa0
simhongsub <oxhe2038@naver.com>
2017-11-02 11:11:13 +0900

    파씽 및 출력 정리

--- getSource.js
+++ getSource.js
@@ -1,27 +1,47 @@
+var xhr = new XMLHttpRequest();
+var clientId = 'z3PC0R7c9QUP9GqOXffK';
+var clientSecret = 'DKRegT6p7Z';
+var resp;
+
 function onWindowLoad() {
   document.getElementById("search").addEventListener("click", search_movie);
 }
 
 function search_movie(){
   var query = document.getElementById('query').value;
-  var xhr = new XMLHttpRequest();
-  var clientId = 'z3PC0R7c9QUP9GqOXffK';
-  var clientSecret = 'DKRegT6p7Z';
-  var resp;
 
-  xhr.open("GET", 'https://openapi.naver.com/v1/search/movie.json' 
+  xhr.open("GET", 'https://openapi.naver.com/v1/search/movie.json'
                 + '?query=' + encodeURI(query) , true);
   xhr.setRequestHeader("X-Naver-Client-Id", clientId);
   xhr.setRequestHeader("X-Naver-Client-Secret", clientSecret);
   xhr.send();
 
   xhr.onreadystatechange = function(){
+    if (chrome.extension.lastError) {
+        document.body.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
+    }else {
+
       if(xhr.readyState == 4 && this.status == 200){
           resp = this.response;
           if (resp !== undefined){
-              document.body.innerText = resp;
-              }
+            obj = JSON.parse(resp);
+            for(i=0;i<obj.total;i++)
+            {
+              //document.write('제목 : ' + obj.items[i].title + '<br/>');
+              document.write('제목 :' + '<a href=""' + obj.items[i].title + '<a/>');
+              document.write('<br/>' + '개봉년도 : ' + obj.items[i].pubDate + '<br/>');
+              document.write('감독 : ' + obj.items[i].director + '<br/>');
+              document.write('배우 : ' + obj.items[i].actor);
+              //var addButton = document.createElement('button');
+              //var addButtonText = document.createTextNode('Add');
+            //  addButton.appendChild(addButtonText);
+              //document.body.appendChild(addButton);
+              document.write('<br/>' + '<br/>');
+            }
           }
+        }
+    }
+
       }
   }
 window.onload = onWindowLoad;
